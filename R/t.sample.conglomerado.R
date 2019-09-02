213c7b6833f5d327bfbb6e95273fd259553fc537
mleal93 <51268889+mleal93@users.noreply.github.com>
2019-09-02 16:58:59 -0300

    Add files via upload

--- R/t.sample.conglomerado.R
+++ R/t.sample.conglomerado.R
@@ -1,5 +1,5 @@
 #' @title Cluster Sampling
-#' 
+#'
 #' @description A function that returns the results from calculated estimated values for Cluster Sampling.
 #'
 #' @import stats
@@ -14,33 +14,44 @@
 #' @export
 
 
-smp.conglomerado = function(data=NULL,c.lev=95,N=NULL,type=NULL,c.unit=NULL) {
-  
-  
+smp.conglomerado = function(data=NULL,c.lev=95,N=NULL,type=NULL,c.unit=NULL,m=NULL) {
+
+
   if( tolower(type)=="p"){
     posx=which(tolower(names(data))=="x")
     posn=which(tolower(names(data))=="n")
-    sample.size=sum(data[,posn])
-    nup=length(data[,1])
-    proporcoes=data[,posx]/data[,posn]
-    p_chapeu=sum(data[,posx])/sample.size
-    p_barra=sum(proporcoes)/nup
-    s2c=(sum((proporcoes-p_barra)^2))/(nup-1)
-    f=sample.size/N
-    v_pbarra=(1-f)*(s2c/nup)
+		if(is.null(m)){
+			n=length(data[,1])
+			m=length(data[,1])
+			proporcoes=data[,posx]/data[,posn]
+			f=(n)/N
+		}else{
+
+			n=data[,posn][1]
+			nup=length(data[,1])
+			proporcoes=data[,posx]/data[,posn]
+			f=(n*m)/N
+
+		}
+
+    p_barra=sum(proporcoes)/m
+    s2c=(sum((proporcoes-p_barra)^2))/(m-1)
+
+
+    v_pbarra=(1-f)*(s2c/m)
     dp_pbarra=sqrt(v_pbarra)
-    if(nup<30){statistic.val     =qt(p=.5+c.lev/200, df = nup-1 );statistic="t Student Statistic"}
+    if(m<30){statistic.val     =qt(p=.5+c.lev/200, df = m-1 );statistic="t Student Statistic"}
     else {statistic.val = qnorm(.5+c.lev/200);statistic="Score Statistic (Z)"}
-    
-  
+
+
   confidence.interval.pbar   =round(p_barra+(c(-1,1)*statistic.val*(dp_pbarra)),6)
-  
-    
+
+
   METHOD = paste("Results for calculated estimatives for Cluster Sampling (Proportion)")
   structure(list(
-    "Sample Size (n)             "    = sample.size,
+    "Sample Size (n)             "    = n,
+    "m                       "    = m,
     "Population Size (N)         "    = N,
-    "p_hat                       "    = p_chapeu,
     "p_bar                       "    = p_barra,
     "Cluster Variance (S2c)      "    = s2c,
     "Sample Fraction (f)         "    = f,
@@ -49,9 +60,10 @@ smp.conglomerado = function(data=NULL,c.lev=95,N=NULL,type=NULL,c.unit=NULL) {
     "Statistic                   "    = statistic,
     "Statistic Value             "    = statistic.val,
     "Confidence Interval of p_bar"    = paste0("IC",c.lev,"% : [ ",confidence.interval.pbar[1]," ; ",confidence.interval.pbar[2]," ]"),
-    
+    "TOTAL :                     "    = p_barra*N,
+    "IC do TOTAL:                "    = paste0("IC",c.lev,"% : [ ",confidence.interval.pbar[1]*N," ; ",confidence.interval.pbar[2]*N," ]"),
     "Proportions                 "    = proporcoes,
-    method=METHOD      
+    method=METHOD
   ),class = "power.htest")
   }else{
     if(c.unit==T){
@@ -59,38 +71,40 @@ smp.conglomerado = function(data=NULL,c.lev=95,N=NULL,type=NULL,c.unit=NULL) {
       rownames(data1)=1:length(data1[,1])
       colnames(data1)=1:length(data1[1,])
       data=as.data.frame(data1)
-      
+
     }
     n=length(data[,1])
     m=length(data[1,])
+    #data2=data[,]
     yi.=apply(data,1,sum)
     total_yi.=sum(yi.)
-    total_yi.2=sum(yi.^2)
-    total_yij=sum(data)^2
+    total_yi.2=sum(yi.**2)
+   # total_yij=sum(data**2)
+    total_yij=sum(data)**2
     y_barra=total_yi./n
     s2b=(total_yi.2-(total_yij/n))/(n-1)
     vchapeu_ybarra=((N-n)/N)*(s2b/n)
     s_ybarra=sqrt(vchapeu_ybarra)
-    
+
     if(n<30){statistic.val     =qt(p=.5+c.lev/200, df = n-1 );statistic="t Student Statistic"}
     else {statistic.val = qnorm(.5+c.lev/200);statistic="Score Statistic (Z)"}
-    
+
     confidence.interval.ybar   =round(y_barra+(c(-1,1)*statistic.val*(s_ybarra)),6)
-    
+
     y_barra2=total_yi./(n*m)
     vchapeu_ybarra2=(1/m^2)*vchapeu_ybarra
     s_ybarra2=sqrt(vchapeu_ybarra2)
     confidence.interval.ybar2   =round(y_barra2+(c(-1,1)*statistic.val*(s_ybarra2)),6)
-    
+
     y_chapeu=N*y_barra
     vchapeu_ychapeu=N^2*vchapeu_ybarra
     s_ychapeu=sqrt(vchapeu_ychapeu)
     confidence.interval.ychapeu   =round(y_chapeu+(c(-1,1)*statistic.val*(s_ychapeu)),6)
-    
-    
+
+
     METHOD = paste("Results for calculated estimatives for Cluster Sampling")
     structure(list(
-      "      #####################       "    =" 
+      "      #####################       "    ="
         Primary Units
       ######################",
       "Statistic                    "    = statistic,
@@ -108,26 +122,24 @@ smp.conglomerado = function(data=NULL,c.lev=95,N=NULL,type=NULL,c.unit=NULL) {
       "vchapeu_ybarra              "    = vchapeu_ybarra,
       "s_ybarra                    "    = s_ybarra,
       "Confidence Interval of y_bar"    = paste0("IC",c.lev,"% : [ ",confidence.interval.ybar[1]," ; ",confidence.interval.ybar[2]," ]"),
-      "      #####################       "    =" 
+      "      #####################       "    ="
         Secondary Units
       ######################",
       "y_barra2                     "    = y_barra2,
       "vchapeu_ybarra2              "    = vchapeu_ybarra2,
       "s_ybarra2                    "    = s_ybarra2,
       "Confidence Interval of y_bar2"    = paste0("IC",c.lev,"% : [ ",confidence.interval.ybar2[1]," ; ",confidence.interval.ybar2[2]," ]"),
-      "      #####################       "    =" 
+      "      #####################       "    ="
         Estimativas para o Total
       ######################",
       "y_chapeu                     "    = y_chapeu,
       "vchapeu_ychapeu              "    = vchapeu_ychapeu,
       "s_ychapeu                    "    = s_ychapeu,
       "Confidence Interval of y_bar2"    = paste0("IC",c.lev,"% : [ ",confidence.interval.ychapeu[1]," ; ",confidence.interval.ychapeu[2]," ]"),
-      
-        method=METHOD                                         
-      
+
+        method=METHOD
+
     ),class = "power.htest")
   }
-  
+
 }
-
-
--- R/t.sample.sistematica.R
+++ R/t.sample.sistematica.R
@@ -1,56 +1,69 @@
-#' @title Sistematic Sampling
-#' 
-#' @description A function that returns the results from calculated estimated values for Sistematic Sampling.
-#'
-#' @import stats
-#' @param data A data frame.
-#' @param c.lev Confidence Level.
-#' @param N Size of population.
-#' @param type Put "p" for estimate values for proportion.
-#' @param c.unit TRUE if want to transpose data frame.
-#'
-#' @author Marco Aurelio Valles Leal
-#'
-#' @export
+  	#'
+	#' @title Sistematic Sampling
+	#' @description A function that returns the results from calculated estimated values for Sistematic Sampling.
+	#'
+	#' @import stats
+	#' @param data A data frame.
+	#' @param c.lev Confidence Level.
+	#' @param N Size of population.
+	#' @param type Put "p" for estimate values for proportion.
+	#' @param c.unit TRUE if want to transpose data frame.
+	#'
+	#' @author Marco Aurelio Valles Leal
+	#'
+	#' @export
 
 
-smp.sistematica = function(data=NULL,c.lev=95,N=NULL,type=NULL,c.unit=NULL) {
-  
-    n=length(data[,1])
-    ni=apply(data,2,length)
-    k=length((data[1,]))
-    x_barra=apply(data,2,mean)
-    s=apply(data,2,sd)
-    y_barra=mean(x_barra)
-    v_ybarra=(1/N)*sum((x_barra-y_barra)^2*ni)
-    ep=sqrt(v_ybarra)
-    
-    if(n<30){statistic.val     =qt(p=.5+c.lev/200, df = n-1 );statistic="t Student Statistic"}
-    else {statistic.val = qnorm(.5+c.lev/200);statistic="Score Statistic (Z)"}
-    confidence.interval.ybarra   =round(y_barra+(c(-1,1)*statistic.val*(ep)),6)
-    
-    
+	smp.sistematica = function(data=NULL,c.lev=95,N=NULL,type=NULL,c.unit=NULL,n=NULL,k=NULL,pi=NULL,np=NULL) {
+  if (is.null(type)){
+	    n=length(data[,1])
+	    ni=apply(data,2,length)
+	    k=length((data[1,]))
+	    x_barra=apply(data,2,mean,na.rm=T)
+	    s=apply(data,2,sd,na.rm=T)
+	    y_barra=mean(x_barra)
+	    v_ybarra=(1/N)*sum((x_barra-y_barra)^2*ni)
+	    ep=sqrt(v_ybarra)
 
-    
-    METHOD = paste("Results for calculated estimatives for Cluster Sampling (Proportion)")
-    structure(list(
-      "Statistic                    "    = statistic,
-      "Statistic Value              "    = statistic.val,
-      "Groups  (k)                  "    = k,
-      "Sample Size (n)              "    = n,
-      "Population Size (N)          "    = N,
-      "Mean                         "    = x_barra,
-      "Y_barra                      "    = y_barra,
-      "Sd                           "    = s,
-      "Var                          "    = s^2,
-      "V(y_barra)                   "    = v_ybarra,
-      "EP                           "     = ep,
-      "Confidence Interval of y_bar2"    = paste0("IC",c.lev,"% : [ ",confidence.interval.ybarra[1]," ; ",confidence.interval.ybarra[2]," ]"),
-      
-      method=METHOD      
-    ),class = "power.htest")
- 
-  
+	    if(TRUE){statistic.val     =qt(p=.5+c.lev/200, df = n-1 );statistic="t Student Statistic"}
+	    else {statistic.val = qnorm(.5+c.lev/200);statistic="Score Statistic (Z)"}
+	    confidence.interval.ybarra   =round(y_barra+(c(-1,1)*statistic.val*(ep)),6)
+
+
+
+
+	    METHOD = paste("Results for calculated estimatives for Cluster Sampling (Proportion)")
+	    structure(list(
+	      "Statistic                    "    = statistic,
+	      "Statistic Value              "    = statistic.val,
+	      "Groups  (k)                  "    = k,
+	      "Sample Size (n)              "    = n,
+	      "Population Size (N)          "    = N,
+	      "Mean                         "    = x_barra,
+	      "Y_barra                      "    = y_barra,
+	      "Sd                           "    = s,
+	      "Var                          "    = s^2,
+	      "V(y_barra)                   "    = v_ybarra,
+	      "EP                           "     = ep,
+	      "Confidence Interval of y_bar2"    = paste0("IC",c.lev,"% : [ ",confidence.interval.ybarra[1]," ; ",confidence.interval.ybarra[2]," ]"),
+
+	      method=METHOD
+	    ),class = "power.htest")
+  }else{
+   
+    statistic.val = qnorm(.5+c.lev/200);statistic="Score Statistic (Z)"
+    p=np/N
+    q=1-p
+    V_ybarra=(((N-n)/(N-1))*((p*q)/n))
+	  V_sist=sum((pi - p)^2)/k
+	  ep=sqrt(V_sist)
+	  ic= round(p+(c(-1,1)*statistic.val*(ep)),6)
+	  cat("V_ybarra(AAS): ",V_ybarra,"\n\n",
+	      "V_sistematica: ",V_sist,"\n\n",
+	      "EP: ",ep,"\n\n",
+	      "IC : [",ic[1],";",ic[2],"]"
+	      )
+
 }
 
-
+	}
